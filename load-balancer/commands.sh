ec0b62079077bce7616e75fd086526fe55c0e5e5
Ed Adkins <ejmadkins@gmail.com>
2020-01-11 17:00:16 +0000

    added load balancer

---
+++ load-balancer/commands.sh
@@ -0,0 +1,29 @@
+# set env variables
+export project_id=ejmadkins-terraform
+export my_zone=europe-west2-a
+export my_cluster=demo-container
+
+# create GKE cluster
+gcloud container clusters create $my_cluster \
+--num-nodes 3 \
+--zone $my_zone \
+--scopes=cloud-platform \
+--enable-ip-alias
+
+# get the credentials
+gcloud container clusters get-credentials $my_cluster
+
+# configure kubectl tab completion
+source <(kubectl completion bash)
+
+# connect to the pod
+kubectl exec -it [POD] -- sh
+
+# install curl
+apk add --no-cache curl
+
+# create a firewall rule to allow a connection to the nodeport
+gcloud compute firewall-rules create test-node-port --allow tcp:60000
+
+# curl from your local machine to the node IP and port
+curl http://[node_ip]:[node_port]
