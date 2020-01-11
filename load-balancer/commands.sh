4761277648e82d284547a9187379829939ddf63f
Ed Adkins <ejmadkins@gmail.com>
2020-01-11 18:34:56 +0000

    added network loadbalancer verification commands

--- load-balancer/commands.sh
+++ load-balancer/commands.sh
@@ -27,3 +27,12 @@ gcloud compute firewall-rules create test-node-port --allow tcp:60000
 
 # curl from your local machine to the node IP and port
 curl http://[node_ip]:[node_port]
+
+# verify the http-health-check
+gcloud compute http-health-checks list
+
+# verify the target-pool
+gcloud compute target-pools list
+
+# verify the forwarding rule
+gcloud compute forwarding-rules list
