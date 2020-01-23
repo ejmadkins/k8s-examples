78d3324ff0db197a30ccaac6e0f67fd0f381ec1c
Ed Adkins <ejmadkins@gmail.com>
2020-01-23 20:30:45 +0000

    added prometheus example

---
+++ prometheus/commands.sh
@@ -0,0 +1,56 @@
+# set env variables
+export project_id=ejmadkins-terraform
+export my_region=europe-west2
+export my_zone=europe-west2-a
+export my_cluster=demo-container
+
+# create GKE cluster
+gcloud container clusters create $my_cluster \
+--num-nodes 3 \
+--zone $my_zone \
+--scopes=cloud-platform \
+--enable-ip-alias
+
+# get the credentials
+gcloud container clusters get-credentials $my_cluster
+
+# configure kubectl tab completion
+source <(kubectl completion bash)
+
+# apply appropriate permissions to deploy Prometheus
+kubectl create clusterrolebinding owner-cluster-admin-binding \
+--clusterrole cluster-admin \
+--user=$(gcloud info --format='value(config.account)')
+
+# create a dedicated namespace
+kubectl create namespace prometheus
+
+# create cluster role
+kubectl create -f cluster-role.yaml
+
+# create cluster role binding
+kubectl create -f cluster-role-binding.yaml
+
+# create ConfigMap
+kubectl create -f configMap.yaml -n prometheus
+
+# create prometheus deployment
+kubectl create -f deployment.yaml -n prometheus
+
+# install the Stackdriver Collector to get those metrics exported to the Stackdriver backend, add env variables
+export KUBE_NAMESPACE=prometheus
+export KUBE_CLUSTER=$my_cluster
+export GCP_REGION=$my_region
+export GCP_PROJECT=$project_id
+export DATA_DIR=/prometheus/
+export DATA_VOLUME=prometheus-storage-volume
+export SIDECAR_IMAGE_TAG=release-0.7.1
+
+# execute script to deploy the stackdriver collector
+sh ./patch.sh deployment prometheus-deployment
+
+# verify that the stackdriver collector is installed
+kubectl -n "${KUBE_NAMESPACE}" get <deployment|statefulset> <name> -o=go-template='{{$output := "stackdriver-prometheus-sidecar does not exists."}}{{range .spec.template.spec.containers}}{{if eq .name "stackdriver-prometheus-sidecar"}}{{$output = (print "stackdriver-prometheus-sidecar exists. Image: " .image)}}{{end}}{{end}}{{printf $output}}{{"\n"}}'
+
+# verify the prometheus deployment
+kubectl get pods -n prometheus
