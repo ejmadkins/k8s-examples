1de5ce93e9f76c4d0070fea345a242561db1ac56
Ed Adkins <ejmadkins@gmail.com>
2020-01-24 09:00:03 +0000

    updated some commands

--- prometheus/commands.sh
+++ prometheus/commands.sh
@@ -1,27 +1,22 @@
-# set env variables
-export project_id=ejmadkins-terraform
-export my_region=europe-west2
-export my_zone=europe-west2-a
-export my_cluster=demo-container
-
 # install the Stackdriver Collector to get those metrics exported to the Stackdriver backend, add env variables
 export KUBE_NAMESPACE=prometheus
-export KUBE_CLUSTER=$my_cluster
-export GCP_REGION=$my_region
-export GCP_PROJECT=$project_id
-export DATA_DIR=/prometheus/
-export DATA_VOLUME=prometheus-storage-volume
-export SIDECAR_IMAGE_TAG=release-0.7.1
+export KUBE_CLUSTER=demo-cluster
+export GCP_ZONE=europe-west2-a
+export GCP_REGION=europe-west2
+export GCP_PROJECT=ejmadkins-anthos-platform
+export DATA_DIR=/data
+export DATA_VOLUME=data-volume
+export SIDECAR_IMAGE_TAG=0.6.4
 
 # create GKE cluster
-gcloud container clusters create $my_cluster \
+gcloud container clusters create $KUBE_CLUSTER \
 --num-nodes 3 \
 --zone $my_zone \
 --scopes=cloud-platform \
 --enable-ip-alias
 
 # get the credentials
-gcloud container clusters get-credentials $my_cluster
+gcloud container clusters get-credentials $KUBE_CLUSTER
 
 # configure kubectl tab completion
 source <(kubectl completion bash)
@@ -54,12 +49,23 @@ kubectl port-forward <pod-name> 8080:9090 -n prometheus
 
 # use web preview in cloud shell
 # create the service
+kubectl apply -f service.yaml
+
+# open firewall ports
+gcloud compute firewall-rules create test-node-port --allow tcp:32387,30182
 
 # execute script to deploy the stackdriver collector
 sh ./patch.sh deployment prometheus-deployment
 
 # verify that the stackdriver collector is installed
-kubectl -n "${KUBE_NAMESPACE}" get <deployment|statefulset> <name> -o=go-template='{{$output := "stackdriver-prometheus-sidecar does not exists."}}{{range .spec.template.spec.containers}}{{if eq .name "stackdriver-prometheus-sidecar"}}{{$output = (print "stackdriver-prometheus-sidecar exists. Image: " .image)}}{{end}}{{end}}{{printf $output}}{{"\n"}}'
+kubectl -n prometheus get deployment prometheus-k8s -o=go-template='{{range .spec.template.spec.containers}}{{if eq .name "stackdriver-prometheus-sidecar"}}{{$output = (print "stackdriver-prometheus-sidecar exists. Image: " .image)}}{{end}}{{printf $output}}{{"\n"}}'
 
 # verify the prometheus deployment
 kubectl get pods -n prometheus
+
+
+kubectl -n prometheus get deployment prometheus-k8s -o=go-template='
+{{range .spec.template.spec.containers}}{{if eq .name "stackdriver-prometheus-sidecar"}}
+{{$output = (print "stackdriver-prometheus-sidecar exists. Image: " .image)}}{{end}}{{end}}{{printf $output}}{{"\n"}}'
+
+
